c5cda9c2ced0c61b7844a49eceb8056f2b849e6b
Leo Cucinell <leocucinell@gmail.com>
2021-08-07 16:35:35 -0700

    added gitignore and readme file

---
+++ server.js
@@ -0,0 +1,11 @@
+/* SECTION: External modules */
+
+/* SECTION: Internal modules */
+
+/* SECTION: Instanced Modules */
+
+/* SECTION: Middleware */
+
+/* SECTION: Routes */
+
+/* SECTION: Port config */
