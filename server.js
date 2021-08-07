bf714d41a96237591566b798f54650951c544123
Leo Cucinell <leocucinell@gmail.com>
2021-08-07 16:50:33 -0700

    created a server file listening on var PORT

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 /* SECTION: External modules */
+const express = require("express");
 
 /* SECTION: Internal modules */
 
 /* SECTION: Instanced Modules */
+const app = express();
 
 /* SECTION: Middleware */
 
 /* SECTION: Routes */
 
 /* SECTION: Port config */
+const PORT = 4000
+app.listen(PORT, (req, res) => {
+    console.log(`App listening on port: ${PORT}`);
+});
