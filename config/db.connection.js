7a61b68a30a0c67228f3e6a26cea3ebfa263e998
Leo Cucinell <leocucinell@gmail.com>
2021-08-07 17:02:18 -0700

    added connection to the database on local machine

---
+++ config/db.connection.js
@@ -0,0 +1,27 @@
+/* SECTION: External modules */
+const mongoose = require("mongoose");
+
+/*NOTE: Set up connection string */
+const connectionStr = "mongodb://localhost:s7017/instapostr";
+
+/* NOTE: Connect to the database */
+mongoose.connect(connectionStr, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+/* SECTION: Test the connection */
+mongoose.connection.on("connected", () => {
+    console.log(`\x1b[36m%s\x1b[0m`, `[${new Date().toLocaleTimeString()}] - MongoDB connected ... 🙌 🙌 🙌`);
+    
+});
+
+mongoose.connection.on("error", () => {
+    console.log('\x1b[31m%s\x1b[0m', 'MongoDB connection error 😥', error);
+});
+
+mongoose.connection.on("disconnected", () => {
+    console.log('\x1b[33m%s\x1b[0m', 'MongoDB disconnected  ⚡️ 🔌 ⚡️')
+});
