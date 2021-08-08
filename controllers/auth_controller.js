8ffcf48d6f8646bec4ddada9b939770ef5c849cd
Leo Cucinell <leocucinell@gmail.com>
2021-08-07 17:38:07 -0700

    stubbed routes for auth

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -5,8 +5,36 @@ const express = require("express");
 const router = express.Router();
 
 /* SECTION: Related model */
+const {User} = require("../models/index");
+
+/* SECTION: Related middleware */
 
 /* SECTION: Routes */
+//GET /login Presentational
+app.get("/login", (req, res, next) => {
+
+});
+
+//POST /login Functional
+app.post("/login", (req, res, next) => {
+
+});
+
+//GET /register Presentational
+app.get("/register", (req, res, next) => {
+
+});
+
+//POST /register Functional
+app.post("/register", (req, res, next) => {
+
+});
+
+//GET /logout Functional
+app.get("/logout", (req, res, next) => {
+
+});
+
 
 /* SECTION: Export */
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 
 /* SECTION: Internal modules */
+const {Auth} = require("./controllers/index");
 
 /* SECTION: Instanced Modules */
 const app = express();
@@ -10,6 +11,7 @@ const app = express();
 app.use("view engine", "ejs");
 
 /* SECTION: Routes */
+app.use("/", Auth);
 
 /* SECTION: Port config */
 const PORT = 4000;
